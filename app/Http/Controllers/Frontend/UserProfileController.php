c953d594fd2e12f016723f1445f9ca870bf43e9e
Jose Felipe Almeida dos Santos <josefelipedev@gmail.com>
2023-10-20 09:04:04 -0300

    feat:Dashboard user profile

--- app/Http/Controllers/Frontend/UserProfileController.php
+++ app/Http/Controllers/Frontend/UserProfileController.php
@@ -5,10 +5,9 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class UserDashboardController extends Controller
+class UserProfileController extends Controller
 {
     public function index(){
-        return view('frontend.dashboard.dashboard');
+        return view('frontend.dashboard.profile');
     }
-
 }
