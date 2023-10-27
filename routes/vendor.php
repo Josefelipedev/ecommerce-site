ed102f41abce7368941365f37eb371211c464b5b
Jose Felipe Almeida dos Santos <josefelipedev@gmail.com>
2023-10-27 09:57:47 -0300

    feat:Vendor profile e password

--- routes/vendor.php
+++ routes/vendor.php
@@ -1,8 +1,14 @@
 <?php
 
 use App\Http\Controllers\Backend\VendorController;
+use App\Http\Controllers\Backend\VendorProfileController;
 use Illuminate\Support\Facades\Route;
 
 
 /** Vendo routes**/
 Route::get('dashboard', [VendorController::class , 'dashboard'])->name('dashboard');
+Route::get('profile', [VendorProfileController::class , 'index'])->name('profile');
+Route::put('profile', [VendorProfileController::class, 'updateVendor'])->name('vendor.update'); // vendor.profile.update
+Route::post('profile', [VendorProfileController::class, 'updatePassword'])->name('vendor.update.password'); // vendor.profile.update
+
+
